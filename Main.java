abe41e922eb16313e2d4b8eecb35af1755fea5a6
RNorris6 <ryan.norris@patelhs.org>
2021-01-29 14:50:19 +0000

    Created 1 print statement and imported the scanner, as well as starting to creat the method for the users favorite soccer team

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+  {
+    Scanner scan = new Scanner(System.in);
+
+    //print statements relating to soccer teams
+    System.out.println("What is your favorite soccer team?");
+    String team = scan.next();
+
+
+
+
+    //Team method returns 
+    Static String team(String team)
+      {
+        
+
+
+      }
+
+
   }
 }
