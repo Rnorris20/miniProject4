d07b171ac75307241336bec65aef5b9668786aee
RNorris6 <ryan.norris@patelhs.org>
2021-02-05 21:22:24 +0000

    Final product, error was an extra curly bracket at the end of line 4

--- Main.java
+++ Main.java
@@ -1,36 +1,39 @@
 import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) 
   {
     Scanner scan = new Scanner(System.in);
-
     //print statements relating to soccer teams
+    System.out.println("Hello, welcome to the soccer shop!");
+    
     System.out.println("What is your favorite soccer team?");
     String team = scan.next();
-    System.out.println("What is your teams score");
-    int teamScore = scan.nextint();
+    System.out.println("What is your teams score?");
+    int teamScore = scan.nextInt();
     System.out.println("What is your opponents score");
-    int opponentScore = scan.nextint();
+    int opponentScore = scan.nextInt();
 
 
-    System.out.println(greeting(team));
     int totalScore = scoreValue(opponentScore, teamScore);
     System.out.println("The point difference between each team is " + totalScore);
    }
 
     //Team method returns 
-    static String soccer(String team)
-      {
-        String soccer = "Your favorite team is " + team;
-        return soccerTeam;
-      }
-
+     
+    static String Soccer(String team)
+     {
+       String soccerTeam = "Your favorite team is " + team;
+       return soccerTeam;
+     }
 
     //Score method returns
     static int scoreValue(int opponentScore, int teamScore)
       {
-        int scoreValue = opponentScore - teamScore;
+        int totalScore = opponentScore - teamScore;
         return totalScore;
       }
   }
+
+
+
